c123fd6a750d4d9abaaac6c7c4ed23f08faaeb3e
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-27 17:31:33 -0600

    Add: getNews function

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,9 +1,21 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 
 const NewsContext = createContext();
 
 export const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState('general');
+  const [news, setNews] = useState({});
+
+  useEffect(() => {
+    const getNews = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${category}&pageSize=10&apiKey=${import.meta.env.VITE_API_KEY}`;
+      const { data } = await axios(url);
+      setNews(data.articles);
+    }
+    getNews();
+  }, [category])
+
 
   const changeCategory = (e) => {
     setCategory(e.target.value);
