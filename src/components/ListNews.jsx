46527f609979ba356032620a3fd538aa93d61214
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-07-28 14:24:02 -0600

    Finished: Pagination

--- src/components/ListNews.jsx
+++ src/components/ListNews.jsx
@@ -1,11 +1,15 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 import useNews from '../hooks/useNews';
 import NewsItem from './NewsItem';
 
 export default function ListNews() {
 
-  const { news } = useNews();
+  const { news, totalNews, changePage, currentPage } = useNews();
+
+  const totalPages = Math.ceil(totalNews / 12);
 
   return (
     <>
@@ -31,6 +35,23 @@ export default function ListNews() {
           />
         ))}
       </Grid>
+
+      <Stack
+        sx={{
+          marginY: 5,
+        }}
+        spacing={2}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Pagination
+          count={totalPages}
+          color="primary"
+          size='medium'
+          page={currentPage}
+          onChange={changePage}
+        />
+      </Stack>
     </>
   )
 }
--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -6,20 +6,40 @@ const NewsContext = createContext();
 export const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState('general');
   const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalNews, setTotalNews] = useState(0);
 
   useEffect(() => {
     const getNews = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=12&apiKey=${import.meta.env.VITE_API_KEY}`;
       const { data } = await axios(url);
       setNews(data.articles);
+      setTotalNews(data.totalResults);
+      setCurrentPage(1);
     }
     getNews();
   }, [category])
+
+  useEffect(() => {
+    const getNews = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&page=${currentPage}&category=${category}&pageSize=12&apiKey=${import.meta.env.VITE_API_KEY}`;
+      const { data } = await axios(url);
+      setNews(data.articles);
+      setTotalNews(data.totalResults);
+    }
+    getNews();
+  }, [currentPage])
 
 
   const changeCategory = (e) => {
     setCategory(e.target.value);
   }
+
+  const changePage = (e, value) => {
+    setCurrentPage(value);
+    window.scrollTo(0, 0);
+  }
+
 
   return (
     <NewsContext.Provider
@@ -27,6 +47,9 @@ export const NewsProvider = ({ children }) => {
         category,
         changeCategory,
         news,
+        totalNews,
+        changePage,
+        currentPage,
       }}
     >
       {children}
